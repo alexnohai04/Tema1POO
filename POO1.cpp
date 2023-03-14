335f9e97ce142203b15f880c56f93f8d6bd2e3c7
alex <alex@DESKTOP-C9ST0IV>
2023-03-15 01:36:33 +0200

    Add project files.

---
+++ POO1.cpp
@@ -0,0 +1,22 @@
+#include "pizza.h"
+
+int main()
+{
+    Ingredient i1("Piper", 0.5, 10), i2("Nimic", 0.0, 0);
+    cout << i1;
+    cin >> i2;
+    i1 += 500; // aduna o valoare la cantitatea disponibila
+    cout << i1;
+    Ingredient i3 = i1, i4, i5(i1);
+    i4 = i1;
+    Pizza pizza1("Clasic"), pizza2;
+/*       pizza1 = i1 + pizza1 // adaugare ingredient 1 in pizza 1
+        cin >> pizza2;
+    cout << pizza1 << pizza2;
+    if (pizza1 > pizza2) cout << "pizza1 are mai multe ingrediente decat pizza2";
+    else cout << "pizza2 are mai multe ingrediente decat pizza1";
+
+
+*/
+    return 0;
+} 
---
+++ pizza.h
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+class Ingredient {
+public:
+	Ingredient() { };
+	Ingredient(string altNume, float altPret, int altaCantitate) {
+		nume = altNume;
+		pret = altPret;
+		cantitate = altaCantitate;
+	}
+	friend ostream& operator <<(ostream& out, const Ingredient& i)
+	{
+		out << i.nume << " " << i.pret << " " << i.cantitate << '\n';
+		return out;
+	}
+	friend istream& operator >>(istream& in, Ingredient& i)
+	{
+		cout << "Nume ingredient:";
+		in >> i.nume;
+		cout << "Pret Ingredient:";
+		in >> i.pret;
+		cout << "Cantitate:";
+		in >> i.cantitate;
+		return in;
+	}
+	Ingredient& operator +=(int var)
+	{
+		cantitate += var;
+		return *this;
+	}
+	Ingredient(const Ingredient& i)
+	{	
+		nume = i.nume;
+		pret = i.pret;
+		cantitate = i.cantitate;
+	}
+	Ingredient& operator =(const Ingredient& i)
+	{
+		nume = i.nume;
+		pret = i.pret;
+		cantitate = i.cantitate;
+	}
+private:
+	string nume;
+	float pret;
+	int cantitate;
+};
+
+
+
+
+class Pizza {
+public:
+	Pizza(){}
+	Pizza(string altaDenumire)
+	{
+		denumire = altaDenumire;
+	}
+	Pizza& operator =(const Pizza& p)
+	{
+		denumire = p.denumire;
+		nrIngrediente = p.nrIngrediente;
+		Ingr = p.Ingr;
+	}
+
+
+
+
+private:
+	string denumire;
+	int nrIngrediente;
+	vector<Ingredient>Ingr;
+};
+
+
+
+
+class Mama {
+public: 
+
+	int getvar() {
+		return var;
+	};
+private: 
+	int var = 6;
+};
